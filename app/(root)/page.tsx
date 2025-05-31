64b190bdaa7596f465254af167afe51edb290191
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-05-31 03:43:25 +0100

    done settingup, working on project now

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import SearchForm from '../components/SearchForm'
 
 const page = () => {
   return (
     <>
-      <h1 className='heading'>HOME</h1>
+      <section className='pink_container'>
+        <h1 className='heading'>Pitch Your Startup, <br /> Connect with /Enterpreneurs</h1>
+        <p className='sub-heading !max-w-3xl'>Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
+        <SearchForm />
+      </section>
     </>
   )
 }
